268998f0b178e2d2875a8e61288b6a04a28982b2
Angel Eduardo Suriaga <angel.suriaga@bayteq.com>
2024-05-13 10:51:31 -0500

    Change create item stats from a loop to listview

--- lib/presentation/ui/screens/home/pokepedia_home_screen.dart
+++ lib/presentation/ui/screens/home/pokepedia_home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pokepedia/presentation/ui/widgets/pokepedia_app_bar.dart';
@@ -14,6 +15,8 @@ class PokepediaHomeScreen extends StatelessWidget {
     final pokepediaBloc = context.read<PokepediaBloc>();
     final pokepediaState = context.watch<PokepediaBloc>().state;
     final height = MediaQuery.of(context).size.height;
+    final toolbarHeight = AppBar().preferredSize.height;
+    final statusBarHeight = MediaQuery.of(context).padding.top;
 
     return Scaffold(
       appBar: const PokepediaAppBar(),
@@ -21,17 +24,22 @@ class PokepediaHomeScreen extends StatelessWidget {
         visible: !pokepediaState.isLoading,
         replacement: const PokepediaLoadingBar(),
         child: RefreshIndicator(
-          onRefresh: () => pokepediaBloc.onRefresh(),
+          onRefresh: pokepediaBloc.onRefresh,
+          backgroundColor: Theme.of(context).colorScheme.secondary,
+          color: Theme.of(context).colorScheme.tertiary,
           child: ListView(
             children: [
               SizedBox(
-                height: height * 0.9,
-                child: Center(
-                  child: PokepediaPageView(
-                    pokemonList: pokepediaState.pokemonList,
-                    pokemonIndexSelected: pokepediaState.pokemonIndexSelected,
-                    onPokemonSelected: pokepediaBloc.onPokemonSelected,
-                    onAbilitySelected: pokepediaBloc.onAbilitySelected,
+                height: (height - toolbarHeight - statusBarHeight),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8),
+                  child: Center(
+                    child: PokepediaPageView(
+                      pokemonList: pokepediaState.pokemonList,
+                      pokemonIndexSelected: pokepediaState.pokemonIndexSelected,
+                      onPokemonSelected: pokepediaBloc.onPokemonSelected,
+                      onAbilitySelected: pokepediaBloc.onAbilitySelected,
+                    ),
                   ),
                 ),
               ),
--- lib/presentation/ui/widgets/pokepedia_page_view.dart
+++ lib/presentation/ui/widgets/pokepedia_page_view.dart
@@ -1,14 +1,10 @@
 import 'package:animate_do/animate_do.dart';
 import 'package:card_swiper/card_swiper.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pokepedia/domain/pokemon.dart';
 import 'package:pokepedia/domain/pokemon_ability.dart';
 import 'package:pokepedia/domain/pokemon_stats.dart';
 import 'package:pokepedia/domain/utils/string_capitalize.dart';
-import 'package:pokepedia/presentation/blocs/pokepedia_bloc.dart';
 import 'package:pokepedia/presentation/ui/widgets/pokepedia_chip.dart';
 
 class PokepediaPageView extends StatefulWidget {
@@ -57,7 +53,7 @@ class _PokepediaPageViewState extends State<PokepediaPageView> {
       onIndexChanged: (value) => widget.onPokemonSelected(value),
       itemBuilder: (context, index) {
         return Padding(
-          padding: const EdgeInsets.only(top: 70),
+          padding: const EdgeInsets.only(top: 55),
           child: PokepediaContent(
             currentPokemon: widget.pokemonList[index],
             onAbilitySelected: (ability) {
@@ -86,7 +82,7 @@ class _PokepediaPaginationCustom extends StatelessWidget {
     const padding = 15.0;
     final itemWidth = (screenWidth - 2 * padding) / 3;
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: padding, vertical: 10),
+      padding: const EdgeInsets.symmetric(horizontal: padding),
       child: ClipRRect(
         borderRadius: BorderRadius.circular(30),
         child: SizedBox(
@@ -139,9 +135,7 @@ class _PokepediaItem extends StatelessWidget {
           borderRadius: BorderRadius.circular(30),
           color: Colors.transparent,
           child: InkWell(
-            onTap: () {
-              onPokemonSelected(index);
-            },
+            onTap: () => onPokemonSelected(index),
             child: Center(
               child: Text(
                 name.capitalize(),
@@ -183,25 +177,28 @@ class PokepediaContent extends StatelessWidget {
             ),
             const SizedBox(height: 10),
             SizedBox(
-              height: 80,
+              height: 95,
               child: _PokepediaSelectedAbilitiesInfo(
                 selectedAbilities: currentPokemon.selectedAbilities,
               ),
             ),
             SizedBox(
-                height: 200,
-                child: _PokepediaImageAndInfo(
-                    pokemonImageUrl: currentPokemon.imageUrl,
-                    pokemonHeight: currentPokemon.height,
-                    pokemonWeight: currentPokemon.weight)),
+              height: 200,
+              child: _PokepediaImageAndInfo(
+                pokemonImageUrl: currentPokemon.imageUrl,
+                pokemonHeight: currentPokemon.height,
+                pokemonWeight: currentPokemon.weight,
+              ),
+            ),
             Divider(
                 thickness: 2, color: Theme.of(context).colorScheme.secondary),
             Expanded(
               child: Padding(
-                padding: const EdgeInsets.only(left: 25, right: 10),
+                padding: const EdgeInsets.only(left: 20, right: 10),
                 child: _PokepediaStatsInfo(
-                    currentStats: currentPokemon.currentStats,
-                    baseStats: currentPokemon.baseStats),
+                  currentStats: currentPokemon.currentStats,
+                  baseStats: currentPokemon.baseStats,
+                ),
               ),
             ),
           ],
@@ -273,7 +270,7 @@ class _PokepediaSelectedAbilitiesInfo extends StatelessWidget {
         const SizedBox(height: 8),
         ...selectedAbilities.map((ability) {
           return Padding(
-            padding: const EdgeInsets.symmetric(vertical: 2),
+            padding: const EdgeInsets.symmetric(vertical: 4),
             child: FadeIn(
               animate: selectedAbilities.contains(ability),
               child: Text(
@@ -335,8 +332,10 @@ class _PokepediaImageAndInfo extends StatelessWidget {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.end,
               children: [
-                Text("Altura: ${pokemonHeight * 10} cm", style: Theme.of(context).textTheme.titleSmall),
-                Text("Peso: ${pokemonWeight * 0.22} lb", style: Theme.of(context).textTheme.titleSmall),
+                Text("Altura: ${pokemonHeight * 10} cm",
+                    style: Theme.of(context).textTheme.titleSmall),
+                Text("Peso: ${pokemonWeight * 0.22} lb",
+                    style: Theme.of(context).textTheme.titleSmall),
               ],
             ),
           )
@@ -356,48 +355,44 @@ class _PokepediaStatsInfo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final colorsScheme = Theme.of(context).colorScheme;
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        ...currentStats.asMap().map((index, stat) {
-          final newStat = stat.toSpanish();
-          final change = newStat.baseStat - baseStats[index].baseStat;
-          final isBaseStat = newStat.baseStat == baseStats[index].baseStat;
-          return MapEntry(
-            index,
-            Column(
+    return ListView.builder(
+      itemCount: currentStats.length,
+      itemBuilder: (context, index) {
+        final newStat = currentStats[index].toSpanish();
+        final change = newStat.baseStat - baseStats[index].baseStat;
+        final isBaseStat = newStat.baseStat == baseStats[index].baseStat;
+        return Column(
+          children: [
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
               children: [
-                const SizedBox(height: 5),
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    SizedBox(
-                      width: 80,
-                      child: Text(
-                        newStat.name,
-                        style: Theme.of(context).textTheme.titleMedium,
-                      ),
-                    ),
-                    const SizedBox(width: 10),
-                    Expanded(
-                      child: Column(
-                        children: [
-                          SizedBox(
-                            height: 30,
-                            child: Stack(
-                              children: [
-                                Positioned(
-                                  bottom: 1,
-                                  left: 1,
-                                  right: 1,
-                                  child: TweenAnimationBuilder<double>(
-                                    tween: Tween<double>(
-                                      begin: 0,
-                                      end: newStat.baseStat / 100,
-                                    ),
-                                    duration: const Duration(milliseconds: 500),
-                                    builder: (context, value, child) =>
-                                        LinearProgressIndicator(
+                SizedBox(
+                  width: 80,
+                  child: Text(
+                    newStat.name,
+                    style: Theme.of(context).textTheme.titleMedium,
+                  ),
+                ),
+                const SizedBox(width: 10),
+                Expanded(
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        height: 30,
+                        child: Stack(
+                          children: [
+                            Positioned(
+                              bottom: 1,
+                              left: 1,
+                              right: 1,
+                              child: TweenAnimationBuilder<double>(
+                                tween: Tween<double>(
+                                  begin: 0,
+                                  end: newStat.baseStat / 100,
+                                ),
+                                duration: const Duration(milliseconds: 500),
+                                builder: (context, value, child) =>
+                                    LinearProgressIndicator(
                                       minHeight: 15,
                                       value: value,
                                       color: Theme.of(context)
@@ -407,46 +402,44 @@ class _PokepediaStatsInfo extends StatelessWidget {
                                           .colorScheme
                                           .secondary,
                                     ),
-                                  ),
-                                ),
-                                Positioned(
-                                  top: 1,
-                                  right: 1,
-                                  child: Text(
-                                    '${change > 0 ? '+' : ''}$change',
-                                    style: TextStyle(
-                                      fontFamily: "PokeFont",
-                                      color: isBaseStat
-                                          ? colorsScheme.tertiary
-                                          : (change > 1
-                                              ? Colors.green
-                                              : colorsScheme.primary),
-                                    ),
-                                  ),
-                                ),
-                              ],
+                              ),
                             ),
-                          ),
-                          const SizedBox(height: 3)
-                        ],
+                            Positioned(
+                              top: 1,
+                              right: 1,
+                              child: Text(
+                                '${change > 0 ? '+' : ''}$change',
+                                style: TextStyle(
+                                  fontFamily: "PokeFont",
+                                  color: isBaseStat
+                                      ? colorsScheme.tertiary
+                                      : (change > 1
+                                      ? Colors.green
+                                      : colorsScheme.primary),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
-                    ),
-                    const SizedBox(width: 10),
-                    SizedBox(
-                      width: 30,
-                      child: Text(
-                        "${stat.baseStat > 100 ? 100 : stat.baseStat}",
-                        style: const TextStyle(
-                            color: Colors.green, fontFamily: "PokeFont"),
-                      ),
-                    ),
-                  ],
+                      const SizedBox(height: 5)
+                    ],
+                  ),
+                ),
+                const SizedBox(width: 10),
+                SizedBox(
+                  width: 30,
+                  child: Text(
+                    "${newStat.baseStat > 100 ? 100 : newStat.baseStat}",
+                    style: const TextStyle(
+                        color: Colors.green, fontFamily: "PokeFont"),
+                  ),
                 ),
               ],
             ),
-          );
-        }).values
-      ],
+          ],
+        );
+      },
     );
   }
 }
